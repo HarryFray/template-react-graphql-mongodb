83f6346b680a0a7a018e559d22f01be5f647101c
HarryFray <harry.fray7@gmail.com>
2018-06-24 17:35:23 -0700

    Add nodemon to watch server

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.all('*', (req, res) => {
   res.json({ ok: true })
+})
+
+
+app.listen(3000, () => {
+  console.log('API local 3000');
 })

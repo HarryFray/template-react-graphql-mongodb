ecad5789dfcb27317347c902585dafea79ac4454
HarryFray <harry.fray7@gmail.com>
2018-06-26 17:19:45 -0700

    Refactor

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # REACT GRAPHQL MONGODB TEMPLATE
-Template for said tech 
-
 
 ```bash
 $ npm install
-$ npm run dev                  # Spins up server on localhost:8080, using webpack-dev-server
+$ npm run dev                  # Spins up server on localhost:8080, along with MongoDB instance
 $ npm run lint                 # airbnb Linter
 $ npm run lintFix              # airbnb Linter auto fix
+```
+
+### Create .env file with DB_PASS=YOUR_MLAB_URI
--- src/server/index.js
+++ src/server/index.js
@@ -1,25 +1,23 @@
 const expressGraphQL = require('express-graphql');
 const express = require('express');
-
 const schema = require('./schema');
-const db = require('./db/index.js');
+const db = require('../db/index.js');
 
 const app = express();
 
+// api
 const apiRouter = express.Router();
-const restRouter = express.Router();
-
 app.use('/api', apiRouter);
+
+// api/rest
+const restRouter = express.Router();
 apiRouter.use('/rest', restRouter);
 
+// api/graphql
 apiRouter.use('/graphql', expressGraphQL({
   schema,
   graphiql: true,
 }));
-
-restRouter.route('/')
-  .get((req, res) => res.json({ getRest: true }))
-  .post((req, res) => res.json({ postRest: true }));
 
 app.listen(8080, () => {
   console.log('API local 8080');
